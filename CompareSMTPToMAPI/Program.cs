acbed5d396b34ea2b69ab9fd3efb791a4626b394
Aaron Gardner <aaron.gardner@yahoo.com>
2015-01-28 00:55:52 -0500

    Working build

--- CompareSMTPToMAPI/Program.cs
+++ CompareSMTPToMAPI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,37 @@ namespace CompareSMTPToMAPI
     {
         static void Main(string[] args)
         {
+            var inputPath = args[0];
+            var df = new DelimitedFile(inputPath, "utf-8");
+            Console.WriteLine("Header fields:");
+            foreach (var fieldName in df.HeaderRecord)
+            {
+                Console.WriteLine(fieldName);
+            }
+            df.GetNextRecord();
+            var outputPath = args[1];
+            var _str = new StreamWriter(outputPath, false, Encoding.UTF8) {AutoFlush = true};
+            while (!df.EndOfFile)
+            {
+                var controlId = df.GetFieldByName("Control ID");
+                var from = df.GetFieldByName("Email From");
+                var to = df.GetFieldByName("Email To");
+                var cc = df.GetFieldByName("Email CC");
+                var bcc = df.GetFieldByName("Email Bcc");
+                var mapiFieldsCharacterCount = to.Length + cc.Length + bcc.Length;
+                var header = df.GetFieldByName("Headers");
+                var smtpHeader = new SmtpHeader(header, "®");
+                var xZantazRecip = smtpHeader.CustomFields.FirstOrDefault(t => t.Item1.Equals("X-Zantaz-Recip:", StringComparison.InvariantCultureIgnoreCase));
+                if (xZantazRecip != null)
+                {
+                    if (xZantazRecip.Item2.Length - mapiFieldsCharacterCount > 1000)
+                    {
+                        _str.WriteLine(controlId);
+                    }
+                }
+                df.GetNextRecord();
+            }
+            _str.Dispose();
         }
     }
 }
---
+++ CompareSMTPToMAPI/SmtpHeader.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace CompareSMTPToMAPI
+{
+    internal class SmtpHeader
+    {
+        #region Constructors
+
+        public SmtpHeader(string header, string newline)
+        {
+            StandardFields = new List<Tuple<string, string>>();
+            CustomFields = new List<Tuple<string, string>>();
+            var headerFields = ParseHeader(header, newline);
+            CustomFields = AssignStandardFields(headerFields);
+        }
+
+        #endregion
+
+        #region Properties
+        public List<Tuple<string, string>> StandardFields { get; private set; } 
+
+        public List<Tuple<string, string>> CustomFields { get; private set; }
+
+        public readonly string[] Rfc822Fields =
+        {
+            "Return-path:",
+            "Received:",
+            "From:",
+            "To:",
+            "CC:",
+            "In-Reply-To:",
+            "Sender:",
+            "Reply-To:",
+            "BCC:",
+            "Message-Id:",
+            "Subject:",
+            "Date:"
+        };
+
+
+
+        #endregion
+
+        #region Methods
+
+        private List<Tuple<string, string>> ParseHeader(string header, string newline)
+        {
+            string[] nlArray = {newline};
+            var explodedHeader = header.Split(nlArray, StringSplitOptions.RemoveEmptyEntries);
+            var headerFields = new List<string>();
+            foreach (var t in explodedHeader)
+            {
+                var str = new StringBuilder();
+                str.Append(t);
+                var initialCharacter = t[0];
+                if (char.IsWhiteSpace(initialCharacter))
+                {
+                    if (headerFields.Count > 0)
+                    {
+                        str.Insert(0, headerFields.Last());
+                        headerFields.RemoveAt(headerFields.Count - 1);
+                    }
+                    headerFields.Add(str.ToString());
+                }
+                else
+                {
+                    headerFields.Add(str.ToString());
+                }
+            }
+            var retVal = new List<Tuple<string, string>>();
+            var components = headerFields.Select(headerField => headerField.Split(new[] {": "}, 2, StringSplitOptions.None)).ToList();
+            foreach (var component in components)
+            {
+                if (component.Length == 1)
+                {
+                    var field = Tuple.Create(string.Format("{0}:", component[0]), string.Empty);
+                    retVal.Add(field);
+                }
+                else if (component.Length == 2)
+                {
+                    var field = Tuple.Create(string.Format("{0}:", component[0]), component[1]);
+                    retVal.Add(field);
+                }
+                else
+                {
+                    throw new InvalidDataException("Failed to parse field into appropriate parts.");
+                }
+            }
+            return retVal;
+        }
+
+        private List<Tuple<string, string>> AssignStandardFields(List<Tuple<string, string>> retVal)
+        {
+            foreach (var field in Rfc822Fields)
+            {
+                var lField = field.ToLowerInvariant();
+                var matches = retVal.Where(t => t.Item1.ToLowerInvariant().Equals(lField)).ToList();
+                if (matches.Count == 0) continue;
+                foreach (var match in matches)
+                {
+                    StandardFields.Add(match);
+                }
+                retVal.RemoveAll(t => t.Item1.ToLowerInvariant().Equals(lField));
+            }
+            return retVal;
+        }
+
+        #endregion
+    }
+}
